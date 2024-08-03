01606ef814ef538fa548ec4052c9f5757e14c10b
Sai Sundeep Rayidi <saisundeep@vt.edu>
2024-08-03 12:47:55 -0400

    Add Model HTTP Methods

--- fastapienv/books2.py
+++ fastapienv/books2.py
@@ -63,6 +63,7 @@ class BookCreateRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=5, max_length=100)
     rating: int = Field(ge=1, le=5)
+    published_date: int = Field(gt=1900, lt=2025)
 
     model_config = {
         "json_schema_extra": {
@@ -71,7 +72,8 @@ class BookCreateRequest(BaseModel):
                     "title": "Round Atlas",
                     "author": "Kevin Plum",
                     "description": "Excellent Novel",
-                    "rating": 5
+                    "rating": 5,
+                    "published_date": 2023
                 }
             ]
         }
@@ -79,12 +81,12 @@ class BookCreateRequest(BaseModel):
 
 
 BOOKS = [
-    {"id": 1, "title": "The Radiant Dawn", "author": "Evelyn Harcourt", "description": "Good book", "rating": 3},
-    {"id": 2, "title": "The Reversal", "author": "Morgan Bellamy", "description": "Good book", "rating": 3},
-    {"id": 3, "title": "The Infinite Horizon", "author": "Jasper Kingsley", "description": "Good book", "rating": 4},
-    {"id": 4, "title": "The Nexus", "author": "Isla Connors", "description": "Great Book", "rating": 4},
-    {"id": 5, "title": "Enchanted Realms", "author": "Lena West", "description": "Great book", "rating": 4},
-    {"id": 6, "title": "The Silent Haven", "author": "Derek Lawson", "description": "Excellent book", "rating": 5}
+    {"id": 1, "title": "The Radiant Dawn", "author": "Evelyn Harcourt", "description": "Good book", "rating": 3, "published_date": 2023},
+    {"id": 2, "title": "The Reversal", "author": "Morgan Bellamy", "description": "Good book", "rating": 3, "published_date": 2022},
+    {"id": 3, "title": "The Infinite Horizon", "author": "Jasper Kingsley", "description": "Good book", "rating": 4, "published_date": 2019},
+    {"id": 4, "title": "The Nexus", "author": "Isla Connors", "description": "Great Book", "rating": 4, "published_date": 2020},
+    {"id": 5, "title": "Enchanted Realms", "author": "Lena West", "description": "Great book", "rating": 4, "published_date": 2021},
+    {"id": 6, "title": "The Silent Haven", "author": "Derek Lawson", "description": "Excellent book", "rating": 5, "published_date": 2022}
 ]
 
 
@@ -96,6 +98,44 @@ def calc_book_id(book: Books):
 @app.get("/books")
 async def get_books():
     return BOOKS
+
+
+@app.get("/books/{book_id}")
+async def read_book(bood_id: int):
+    for book in BOOKS:
+        if book.get("id") == book_id:
+            return book
+
+@app.get("/books/")
+async def read_books_by_rating(book_rating: int):
+    result_books = []
+    for book in BOOKS:
+        if book.get("rating") == book_rating:
+            result_books.append(book)
+    return result_books
+
+
+@app.get("/books/update_book")
+async def update_book_by_id(book: BookCreateRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+            break
+
+@app.get("/books/published_date/{pub_date}")
+async def read_book_by_published_date(pub_date: int):
+    result_books = []
+    for book in BOOKS:
+        if book.get("publication_date") == pub_date:
+            result_books.append(book)
+    return result_books
+
+@app.delete("/books/{book_id}")
+async def delete_a_book(book_id: int):
+    for i in range(len(BOOKS)):
+        if book.get("id") == book_id:
+            BOOKS.pop(i)
+            break
 
 
 @app.post("/create_book")
